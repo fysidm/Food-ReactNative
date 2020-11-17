7b8630d8662a22ba8ac20c01c914d94d66fc529e
GeorgeWu <fysidm@gmail.com>
2020-11-17 16:02:25 +0800

    Fix bugs

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    'Authorization': `Bearer ${process.env.YELP_API_KEY}`
   }
 });
